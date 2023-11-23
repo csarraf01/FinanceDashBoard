78c106a31e5de5ec8065dc86475117364980f945
csarraf01 <chiragsarraf98@gmail.com>
2023-11-23 17:15:23 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,43 @@
+import { ColorModeContext, useMode } from "./theme";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { Routes, Route } from "react-router-dom";
+import Topbar from "./pages/global/topbar";
+import Dashboard from "./pages/Dashboard";
+import Navbar from "./pages/global/Navbar";
+import Team from "./pages/Team";
+import Contacts from "./pages/Contacts";
+import Invoices from "./pages/Invoices";
+import Profile from "./pages/Profile";
+import FAQ from "./pages/faq";
+import Bar from "./pages/Charts/Bar";
+import Pie from "./pages/Charts/Pie";
+import Line from "./pages/Charts/Line";
+import Geography from "./pages/Charts/Geography";
+export default function App() {
+  const [theme, colorMode] = useMode();
+  return (
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <div className="app">
+          <Navbar />
+          <main className="content">
+            <Topbar />
+            <Routes>
+              <Route path="/" element={<Dashboard />} />
+              <Route path="/team" element={<Team />} />
+              <Route path="/contacts" element={<Contacts />} />
+              <Route path="/invoices" element={<Invoices />} />
+              <Route path="/profile" element={<Profile />} />
+              <Route path="/faq" element={<FAQ />} />
+              <Route path="/bar" element={<Bar />} />
+              <Route path="/pie" element={<Pie />} />
+              <Route path="/line" element={<Line />} />
+              <Route path="/geography" element={<Geography />} />
+            </Routes>
+          </main>
+        </div>
+      </ThemeProvider>
+    </ColorModeContext.Provider>
+  );
+}
